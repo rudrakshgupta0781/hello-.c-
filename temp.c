54cf756727e69deadb9dcf2bc16112273e6087ad
rudrakshgupta0781 <101164815+rudrakshgupta0781@users.noreply.github.com>
2025-08-23 00:45:35 +0530

    Add files via upload

---
+++ temp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+  int main(){
+    float temprature;
+    printf("enter temprature in celcius ");
+    scanf("%f", &temprature);
+    printf("temprature in farenheit is %f", (temprature * 9/5) + 32);
+
+
+
+
+
+
+    return 0 ;
+  }
