7173d483126c3f19de8ee28e72853ef0c5c9bbe3
rudrakshgupta0781 <101164815+rudrakshgupta0781@users.noreply.github.com>
2025-08-27 00:16:40 +0530

    Add files via upload

---
+++ swap without variable .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+   int main() {
+    int a , b ;
+    printf("enter a ");
+    scanf("%d", &a);
+    printf("enterb");
+    scanf("%d",&b);
+     printf("Before swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+// after swapping 
+printf("After swapping:\n");
+printf("a = %d\n",b);
+printf("b=%d\n",a);
+
+
+
+
+
+return 0 ; 
+   }
