19b22b9c69fae60bb7159a74b0721e8fbe963da4
rudrakshgupta0781 <101164815+rudrakshgupta0781@users.noreply.github.com>
2025-08-30 01:38:11 +0530

    Add files via upload

--- leap year .c
+++ leap year .c
@@ -1,17 +1,19 @@
 #include<stdio.h>
   int main(){
-    int year ; 
-    printf("enter a year :");
-    scanf("%d",&year);
-   if( year%4==0){
-printf("leap year");
-   }
-   else {
-    printf("not a leap year");
+    char alpha;
+    printf("enter a alphabet:");
+    scanf("%c",&alpha);
+    if(alpha=='a'|| alpha=='e'|| alpha=='i'|| alpha=='o'|| alpha=='u'|| alpha=='A'|| alpha=='E'|| alpha=='I'|| alpha=='O'){
+      printf("vowel");
+    }
+    else if ( alpha>='A'&& alpha<='Z'|| alpha>='a'&& alpha<='z'){
     
+      printf("consonant");
+    
+    }
+   else {
+    printf("not an alphabet");
    }
-
-   
 
 
 
