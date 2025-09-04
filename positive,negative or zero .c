48517fd8cbd56c43fb5d84a67786894ea89c11ba
rudrakshgupta0781 <101164815+rudrakshgupta0781@users.noreply.github.com>
2025-09-04 23:24:39 +0530

    Add files via upload

---
+++ positive,negative or zero .c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+int number ; 
+printf("Emter a nuber :");
+scanf("%d",&number);
+if(number>0){
+  printf("The number is positive ");
+
+}
+if (number<0){
+  printf("The number is negative ");
+}
+
+else if (number==0){
+  printf("The number is zero");
+}
+
+
+
+
+
+
+  return 0 ; 
+}
+  
